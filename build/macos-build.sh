7903d51dac69a1b973cfde96107df5f30cfaba2c
Calvin Cheng <linchuan.cheng@gmail.com>
2022-03-17 23:36:11 +0800

    macos and ios builds

--- build/macos-build.sh
+++ build/macos-build.sh
@@ -1,8 +1,76 @@
-#!/bin/bash
+BUILD_IOS=
+BUILD_OSX=
+
+unknownParameter()
+{
+    if [[ -n $2 &&  $2 != "" ]]; then
+        echo Unknown argument \"$2\" for parameter $1.
+    else
+        echo Unknown argument $1
+    fi
+    die
+}
+
+parseArgs()
+{
+    while [ "$1" != "" ]; do
+        case $1 in
+
+        	-ios)
+                BUILD_IOS=1
+                ;;
+
+            -osx)
+                BUILD_OSX=1
+                ;;
+
+            --min-ios-version)
+                if [ -n $2 ]; then
+                    SDKVERSION=$2
+                    shift
+                else
+                    missingParameter $1
+                fi
+                ;;
+			--min-macosx-version)
+				if [ -n $2 ]; then
+					SDKVERSION=$2
+					shift
+				else
+					missingParameter $1
+				fi
+				;;
+
+            *)
+                unknownParameter $1
+                ;;
+        esac
+
+        shift
+    done
+}
+
+parseArgs $@
+
+
+if [[ -z $BUILD_IOS && -z $BUILD_OSX ]]; then
+	BUILD_IOS=1
+fi
+
+CURRENT=`pwd`
+# if [ -d "${CURRENT}/gmp/gmplib" ]; then
+# 	exit
+# fi
 
 __pr="--print-path"
 __name="xcode-select"
 DEVELOPER=`${__name} ${__pr}`
+
+GMP_VERSION="6.2.1"
+
+if [[ -z $SDKVERSION ]]; then
+    SDKVERSION=`xcrun -sdk iphoneos --show-sdk-version`
+fi
 
 OSX_PLATFORM=`xcrun --sdk macosx --show-sdk-platform-path`
 OSX_SDK=`xcrun --sdk macosx --show-sdk-path`
@@ -18,35 +86,24 @@ CLANGPP=`xcrun --sdk iphoneos --find clang++`
 
 BITCODE_FLAGS=" -disable-llvm-optzns -O3"
 
-mkdir -p gmp
-cd gmp
-CURRENT=`pwd`
+downloadGMP()
+{
+    if [ ! -d "${CURRENT}/gmp" ]; then
+        echo "Downloading GMP"
+        curl -L -o "${CURRENT}/gmp-${GMP_VERSION}.tar.bz2" http://ftp.gnu.org/gnu/gmp/gmp-${GMP_VERSION}.tar.bz2
+		tar xfj "gmp-${GMP_VERSION}.tar.bz2"
+		mv gmp-${GMP_VERSION} gmp
+		rm "gmp-${GMP_VERSION}.tar.bz2"
+    fi
+}
 
 build()
 {
-    case $1 in
-        arm64-ios)
-            ARCH=arm64
-            SDK=${IPHONEOS_SDK}
-            PLATFORM=${IPHONEOS_PLATFORM}
-            ARGS=""
-            TYPE=ios
-            ;;
-        x86_64-ios)
-            ARCH=x86_64
-            SDK=${IPHONESIMULATOR_SDK}
-            PLATFORM=${IPHONESIMULATOR_PLATFORM}
-            ARGS=""
-            TYPE=ios
-            ;;
-        *)
-            ARCH=x86_64
-            SDK=${OSX_SDK}
-            PLATFORM=${OSX_PLATFORM}
-            ARGS="ABI=64"
-            TYPE=osx
-            ;;
-    esac
+	ARCH=$1
+	SDK=$2
+	PLATFORM=$3
+	ARGS=$4
+	TYPE=$5
  
 	make clean &> "${CURRENT}/clean.log"
 	make distclean &> "${CURRENT}/clean.log"
@@ -89,4 +146,53 @@ build()
 	make install &> "${CURRENT}/gmplib-${ARCH}-${TYPE}-install.log"
 }
 
-build $1
+downloadGMP
+
+cd gmp
+CURRENT=`pwd`
+
+echo "GMP $CURRENT"
+
+if [[ -n $BUILD_IOS ]]; then
+#    build "armv7" "${IPHONEOS_SDK}" "${IPHONEOS_PLATFORM}" "" "ios"
+	# build "armv7s" "${IPHONEOS_SDK}" "${IPHONEOS_PLATFORM}" "" "ios"
+	build "arm64" "${IPHONEOS_SDK}" "${IPHONEOS_PLATFORM}" "" "ios"
+#    build "i386" "${IPHONESIMULATOR_SDK}" "${IPHONESIMULATOR_PLATFORM}" "" "ios"
+	build "x86_64" "${IPHONESIMULATOR_SDK}" "${IPHONESIMULATOR_PLATFORM}" "" "ios"
+
+	[ -d gmplib/ios ] || mkdir -p gmplib/ios
+	cp -r "${CURRENT}/gmplib-arm64-ios/include" gmplib/include
+
+	echo "Creating Universal library"
+
+    lipo \
+        "${CURRENT}/gmplib-arm64-ios/lib/libgmp.a" \
+        "${CURRENT}/gmplib-x86_64-ios/lib/libgmp.a" \
+        -create -output "${CURRENT}/gmplib/ios/libgmp.a"
+
+#        "${CURRENT}/gmplib-armv7-ios/lib/libgmp.a" 
+#        "${CURRENT}/gmplib-i386-ios/lib/libgmp.a"
+
+fi
+
+if [[ -n $BUILD_OSX ]]; then
+#    build "i386" "${OSX_SDK}" "${OSX_PLATFORM}" "ABI=32" "osx"
+	build "x86_64" "${OSX_SDK}" "${OSX_PLATFORM}" "ABI=64" "osx"
+	# maybe support arm64 later
+
+	[ -d gmplib/osx ] || mkdir -p gmplib/osx
+	cp -r "${CURRENT}/gmplib-x86_64-osx/include" gmplib/include
+
+	echo "Creating Universal library"
+
+    cp "${CURRENT}/gmplib-x86_64-osx/lib/libgmp.a" "${CURRENT}/gmplib/osx/libgmp.a"
+	
+	# modify this to support arm64 later
+#    lipo \
+#        "${CURRENT}/gmplib-i386-osx/lib/libgmp.a" \
+#        "${CURRENT}/gmplib-x86_64-osx/lib/libgmp.a" \
+#        -create -output "${CURRENT}/gmplib/osx/libgmp.a"
+fi
+
+
+echo "Done with GMP!"
